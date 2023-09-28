100e84c5c38af07a4585cc1c19bd05d2d16dc85a
Apricity <89236552+ApricityZ@users.noreply.github.com>
2023-09-28 16:55:37 +0800

    2

--- BasicDataType.py
+++ BasicDataType.py
@@ -10,3 +10,19 @@ print(counter)
 print(miles)
 print(name)
 
+
+a = b = c = 1
+
+a, b, c = 1, 2, "runnoob"
+
+a = 111
+print(isinstance(a, int))
+
+'''
+type() 和 isinstance 的区别是：
+    type不会认为子类是一种父类类型
+    而isinstance会认为子类是一种父类类型
+'''
+
+var = 1
+del var
